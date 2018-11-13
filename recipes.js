e32b7a3e3d4fad8c5271f12af8f02b8f13beaa2e
mannysin <sinclair2610@gmail.com>
2018-11-13 15:55:44 -0500

    need to finish iteration 3 on

--- recipes.js
+++ recipes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Recipe = require('./Recipe');
 const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
@@ -8,3 +8,27 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+  const theRecipe = new Recipe({title: "yoyoyo"})    
+
+  theRecipe.save()
+  .then((theThingIGetBackFromDB)=>{
+
+      console.log(theThingIGetBackFromDB);
+
+  })
+  .catch((theErrorThing)=>{
+
+      console.log(theErrorThing);
+      
+  })
+
+// Recipes.create({title: "yo"})
+// .then((theSingleCat)=> {
+//   console.log(theSingleCat)
+// })
+// .catch((err)=> {
+
+//     console.log(err);
+    
+// })
